85a7054342ed001c8e2b53754068dae01c73f8bb
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-12-23 14:09:40 +0530

    feat: ci cd for this project deploy s3

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 
 import "./tailwind.css";
+import styles from '~/styles/main.css';
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -19,7 +20,8 @@ export const links: LinksFunction = () => [
   {
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
+  }, 
+  { rel: 'stylesheet', href: styles }
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
