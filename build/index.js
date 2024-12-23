63928a271a8c5884cf2220bfacdc9ea8dcc71866
Abhishek Jaiswal <iam001abhi@gmail.com>
2024-12-23 14:10:04 +0530

    feat: ci cd for this project deploy s3

---
+++ build/index.js
@@ -0,0 +1,242 @@
+var __defProp = Object.defineProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: !0 });
+};
+
+// app/entry.server.tsx
+var entry_server_exports = {};
+__export(entry_server_exports, {
+  default: () => handleRequest
+});
+import { PassThrough } from "node:stream";
+import { createReadableStreamFromReadable } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import { isbot } from "isbot";
+import { renderToPipeableStream } from "react-dom/server";
+import { jsx } from "react/jsx-runtime";
+var ABORT_DELAY = 5e3;
+function handleRequest(request, responseStatusCode, responseHeaders, remixContext, loadContext) {
+  return isbot(request.headers.get("user-agent") || "") ? handleBotRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
+  ) : handleBrowserRequest(
+    request,
+    responseStatusCode,
+    responseHeaders,
+    remixContext
+  );
+}
+function handleBotRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = !1, { pipe, abort } = renderToPipeableStream(
+      /* @__PURE__ */ jsx(
+        RemixServer,
+        {
+          context: remixContext,
+          url: request.url,
+          abortDelay: ABORT_DELAY
+        }
+      ),
+      {
+        onAllReady() {
+          shellRendered = !0;
+          let body = new PassThrough(), stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html"), resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode
+            })
+          ), pipe(body);
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          responseStatusCode = 500, shellRendered && console.error(error);
+        }
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+function handleBrowserRequest(request, responseStatusCode, responseHeaders, remixContext) {
+  return new Promise((resolve, reject) => {
+    let shellRendered = !1, { pipe, abort } = renderToPipeableStream(
+      /* @__PURE__ */ jsx(
+        RemixServer,
+        {
+          context: remixContext,
+          url: request.url,
+          abortDelay: ABORT_DELAY
+        }
+      ),
+      {
+        onShellReady() {
+          shellRendered = !0;
+          let body = new PassThrough(), stream = createReadableStreamFromReadable(body);
+          responseHeaders.set("Content-Type", "text/html"), resolve(
+            new Response(stream, {
+              headers: responseHeaders,
+              status: responseStatusCode
+            })
+          ), pipe(body);
+        },
+        onShellError(error) {
+          reject(error);
+        },
+        onError(error) {
+          responseStatusCode = 500, shellRendered && console.error(error);
+        }
+      }
+    );
+    setTimeout(abort, ABORT_DELAY);
+  });
+}
+
+// app/root.tsx
+var root_exports = {};
+__export(root_exports, {
+  Layout: () => Layout,
+  default: () => App,
+  links: () => links
+});
+import {
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "@remix-run/react";
+
+// app/styles/main.css
+var main_default = "/build/_assets/main-OVMWVYWS.css";
+
+// app/root.tsx
+import { jsx as jsx2, jsxs } from "react/jsx-runtime";
+var links = () => [
+  { rel: "preconnect", href: "https://fonts.googleapis.com" },
+  {
+    rel: "preconnect",
+    href: "https://fonts.gstatic.com",
+    crossOrigin: "anonymous"
+  },
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+  },
+  { rel: "stylesheet", href: main_default }
+];
+function Layout({ children }) {
+  return /* @__PURE__ */ jsxs("html", { lang: "en", children: [
+    /* @__PURE__ */ jsxs("head", { children: [
+      /* @__PURE__ */ jsx2("meta", { charSet: "utf-8" }),
+      /* @__PURE__ */ jsx2("meta", { name: "viewport", content: "width=device-width, initial-scale=1" }),
+      /* @__PURE__ */ jsx2(Meta, {}),
+      /* @__PURE__ */ jsx2(Links, {})
+    ] }),
+    /* @__PURE__ */ jsxs("body", { children: [
+      children,
+      /* @__PURE__ */ jsx2(ScrollRestoration, {}),
+      /* @__PURE__ */ jsx2(Scripts, {})
+    ] })
+  ] });
+}
+function App() {
+  return /* @__PURE__ */ jsx2(Outlet, {});
+}
+
+// app/routes/_index.tsx
+var index_exports = {};
+__export(index_exports, {
+  default: () => Index,
+  links: () => links2
+});
+import { Link } from "@remix-run/react";
+
+// app/styles/home.css
+var home_default = "/build/_assets/home-K2WXCKZZ.css";
+
+// app/routes/_index.tsx
+import { jsx as jsx3, jsxs as jsxs2 } from "react/jsx-runtime";
+function Index() {
+  return /* @__PURE__ */ jsxs2("main", { id: "content", children: [
+    /* @__PURE__ */ jsx3("h1", { children: "A better way of keeping track of your notes" }),
+    /* @__PURE__ */ jsx3("p", { children: "Try our early beta and never loose track of your notes again!" }),
+    /* @__PURE__ */ jsx3("p", { id: "cta", children: /* @__PURE__ */ jsx3(Link, { to: "/notes", children: "Try Now!" }) })
+  ] });
+}
+function links2() {
+  return [{ rel: "stylesheet", href: home_default }];
+}
+
+// app/routes/notes.tsx
+var notes_exports = {};
+__export(notes_exports, {
+  default: () => NotesPage
+});
+
+// app/components/NewNote.jsx
+import { jsx as jsx4, jsxs as jsxs3 } from "react/jsx-runtime";
+function NewNote() {
+  return /* @__PURE__ */ jsxs3("form", { method: "post", id: "note-form", children: [
+    /* @__PURE__ */ jsxs3("p", { children: [
+      /* @__PURE__ */ jsx4("label", { htmlFor: "title", children: "Title" }),
+      /* @__PURE__ */ jsx4("input", { type: "text", id: "title", name: "title", required: !0 })
+    ] }),
+    /* @__PURE__ */ jsxs3("p", { children: [
+      /* @__PURE__ */ jsx4("label", { htmlFor: "content", children: "Content" }),
+      /* @__PURE__ */ jsx4("textarea", { id: "content", name: "content", rows: "5", required: !0 })
+    ] }),
+    /* @__PURE__ */ jsx4("div", { className: "form-actions", children: /* @__PURE__ */ jsx4("button", { children: "Add Note" }) })
+  ] });
+}
+var NewNote_default = NewNote;
+
+// app/routes/notes.tsx
+import { jsx as jsx5 } from "react/jsx-runtime";
+function NotesPage() {
+  return /* @__PURE__ */ jsx5("main", { children: /* @__PURE__ */ jsx5(NewNote_default, {}) });
+}
+
+// server-assets-manifest:@remix-run/dev/assets-manifest
+var assets_manifest_default = { entry: { module: "/build/entry.client-XK5AXWBN.js", imports: ["/build/_shared/chunk-HXPP5Q5P.js", "/build/_shared/chunk-MFPRU5OA.js"] }, routes: { root: { id: "root", parentId: void 0, path: "", index: void 0, caseSensitive: void 0, module: "/build/root-OWTY4QGT.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 }, "routes/_index": { id: "routes/_index", parentId: "root", path: void 0, index: !0, caseSensitive: void 0, module: "/build/routes/_index-36OTI33F.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 }, "routes/notes": { id: "routes/notes", parentId: "root", path: "notes", index: void 0, caseSensitive: void 0, module: "/build/routes/notes-JWHHNTW6.js", imports: void 0, hasAction: !1, hasLoader: !1, hasClientAction: !1, hasClientLoader: !1, hasErrorBoundary: !1 } }, version: "750e9667", hmr: void 0, url: "/build/manifest-750E9667.js" };
+
+// server-entry-module:@remix-run/dev/server-build
+var mode = "production", assetsBuildDirectory = "public/build", future = { v3_fetcherPersist: !1, v3_relativeSplatPath: !1, v3_throwAbortReason: !1, v3_routeConfig: !1, v3_singleFetch: !1, v3_lazyRouteDiscovery: !1, unstable_optimizeDeps: !1 }, publicPath = "/build/", entry = { module: entry_server_exports }, routes = {
+  root: {
+    id: "root",
+    parentId: void 0,
+    path: "",
+    index: void 0,
+    caseSensitive: void 0,
+    module: root_exports
+  },
+  "routes/_index": {
+    id: "routes/_index",
+    parentId: "root",
+    path: void 0,
+    index: !0,
+    caseSensitive: void 0,
+    module: index_exports
+  },
+  "routes/notes": {
+    id: "routes/notes",
+    parentId: "root",
+    path: "notes",
+    index: void 0,
+    caseSensitive: void 0,
+    module: notes_exports
+  }
+};
+export {
+  assets_manifest_default as assets,
+  assetsBuildDirectory,
+  entry,
+  future,
+  mode,
+  publicPath,
+  routes
+};
